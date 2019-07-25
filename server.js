501c3dc6b93887d0bf31bf41adcc37fbd5a4f16f
youngheart12 <manishkumar199222@gmail.com>
2019-07-25 08:23:26 +0530

    new changes

--- server.js
+++ server.js
@@ -30,13 +30,15 @@ mongoose.connect(db,{
 app.use('/api/items', require('./routes/api/items'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'))
-const port=process.env.PORT||5000
-
-if(process.env.NODE_ENV === 'production')
-{
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
-app.listen(port,()=>console.log(`server started on ${port}`))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const port = process.env.PORT || 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`));
